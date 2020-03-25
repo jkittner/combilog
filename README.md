567f3795b0c09984d7f71bba5ae12351ed44ed10
Jonas Kittner <jonas.kittner@rub.de>
2020-03-25 19:10:50 +0100

    Added more function - used type annotations - fixed mistake in function description str - there is a known bug where ° is not recognized as an ascii character on windows - cleaned up readme

--- README.md
+++ README.md
@@ -1,26 +1,19 @@
 # Downloading logged data from Theodor Friedrichs & Co Combilog 1022
 
-## Intention for writing this code
-The intention for writing this code was the lack of  affordable opportunities offered by Theodor Friedrichs for automatically downloading the data from the datalogger.
-Also there still is no software for Linux or for servers without a GUI.
-This script should run on all of them, they just need Python3.
-This software should partitially do what the Comgraph software does which only runs on windows and is not free.
-
-
-##### Disclaimer
-I am not a professional programmer at all, but I try to do my best to get things running the way I want them to - and sometimes it even works! 
-So feel free to copy my messy code and improve it until it suits your needs.
-
 ## Usage
-The file `combilog_FUN.py` includes all the classes, functions, class objects and classmethods you will need to communicate with the logger.
+The file `combilog_FUN.py` includes all classes you will need to communicate with the logger.
 
 The file `combilog_read_data.py` includes basic examples how to use the classes.
 
-The class object `other_call`from the class `combilog` offers you the option to create any call as described in the logger manual using this class and the initialized logger. The commonly used calls have their own class object in the class `combilog`.
+The class object `other_call` from the class `combilog` offers you the option to create any call as described in the logger manual using this class and the initialized logger. The commonly used calls have their own class object in the class `combilog`.
 
 The manual can be found [here](http://www.th-friedrichs.de/assets/ProductPage/ProductDownload/ManualE1022V109.pdf). The `ASCII` protocol is described starting at page 118.
 
 ### My Usage
-I personally use this for my private weatherstation. The logger is connected via USB to a Raspberry Pi running a basic rasbian. Every 5 minutes - when a log was written - I fetch the data from the logger and save it directly into a PostgreSQL database.
-All other automated scripts get the latest data out of the database. The data analysis is done on the Pi and all plots are created using `R`.
-All graphs are hosted on a website using `apache` on the Raspberry Pi.
+I personally use this for my private weatherstation. The logger is connected via USB to a Raspberry Pi running a basic rasbian. Every 5 minutes when a log was written I fetch the data from the logger and save it directly into a PostgreSQL database.
+
+### Intention for writing this code
+The intention for writing this code was the lack of affordable opportunities offered by Theodor Friedrichs for automatically downloading the data from the datalogger.
+Also there still is no software for Linux or for servers without a GUI.
+This script should run on all of them, they just need python3.
+This software should do what the autmatic part of the Comgraph software does which only runs on windows and is not free.
--- combilog_FUN.py
+++ combilog_FUN.py
@@ -1,18 +1,17 @@
 # classes for combilog_read_data.py
 # Github: theendlessriver13/combilog1022
-# (C) Jonas Kittner 2019
+# Jonas Kittner 2020
 
+import serial
+import pandas   as pd
 from   datetime import datetime
 from   struct   import unpack
-import pandas   as pd
 
 class telegram:
 
     @classmethod
-    def createTG(cls, ser, address, *argv):
-        '''
-        build telegram
-        '''
+    def createTG(cls, ser: serial.Serial, address: str, *argv: str) -> bytes:
+        '''build a telegram an make a call'''
         tgStr = '$' + address
         for arg in argv:
             tgStr += arg
@@ -23,76 +22,64 @@ class telegram:
         return re
 
     @classmethod
-    def checkRe(cls, answer):
-        '''
-        Check answer of calls that return only NAK or ACK
-        '''
+    def checkRe(cls, answer: bytes) -> None:
+        '''Check answer of calls that return only NAK or ACK'''
         if   (answer == b'\x15'):
-            raise Exception('NAK - Call not succesessfull')
+            raise Exception('NAK - call not successful')
         elif (answer == b'\x06'):
-            print('ACK - Call succesessfull')
+            print('ACK - call successful')
         else:
-            raise Exception('unknown Byte returned')
+            raise Exception('unknown byte returned')
 
     @classmethod
-    def hexIEE_dec(cls, hexval):
-        '''
-        decode the data read from the logger's storage drive
-        '''
-        return round(unpack('!f', bytes.fromhex(hexval))[0], 2)
+    def hexIEE_dec(cls, hexval: str) -> float:
+        '''decode the data read from the logger's storage drive'''
+        dec = round(unpack('!f', bytes.fromhex(hexval))[0], 2)
+        return dec
 
 
 class combilog:
 
-    def __init__(self, logger_params):
+    def __init__(self, logger_params: dict) -> None:
         self.address = logger_params['address']
         self.passwd  = logger_params['passwd']
         self.ser     = logger_params['ser']
         self.cnames  = logger_params['cnames']
-    
-    def auth(self):
-        '''
-        autheticate with password
-        '''
+
+    def auth(self) -> None:
+        '''autheticate with password to set pointers or delete storage'''
         tgStr    = '$' + self.address + 'P' + self.passwd + '\r'
         tgByte   = tgStr.encode('utf-8')
         self.ser.write(tgByte)
         passwdRe = self.ser.read_until(b'\r')
         telegram.checkRe(passwdRe)
 
-    def pointer1_tostart(self):
-        '''
-        sets pointer1 to start
-        '''
+    def pointer1_tostart(self) -> None:
+        '''sets pointer 1 to start'''
         tg = telegram.createTG(self.ser, self.address, 'C')
         telegram.checkRe(tg)
-    
-    def pointer1_to_date(self, date):
-        '''
-        sets pointer 1 to a specific date and time
-        date must be str with format "%y%m%d%H%M%S"
-        '''
+
+    def pointer1_to_date(self, date: datetime) -> None:
+        '''sets pointer 1 to a specific date and time'''
         date = datetime.strftime(date, '%y%m%d%H%M%S')
         tg   = telegram.createTG(self.ser, self.address, 'C', date)
         telegram.checkRe(tg)
 
-    def nr_logs(self):
-        '''
-        checks how many logs are available with the currently set pointer
-        '''
-        logs = int(telegram.createTG(self.ser, self.address, 'N')[1:])
-        return logs
+    def pointer1_to_pos(self, pos: str) -> None:
+        '''sets pointer 1 to a specific position'''
+        tg = telegram.createTG(self.ser, self.address, 'C', pos)
+        telegram.checkRe(tg)
 
-    def read_bookings_pt1(self):
+    def read_bookings_pt1(self, arg: str = 'E') -> pd.DataFrame:
         '''
-        reads all bookings for the currently set pointer
+        reads all bookings for the currently set pointer 1
         returns a pandas dataframe with all data
         '''
         i    = 0
         df   = pd.DataFrame(columns = self.cnames)
         logs = int(telegram.createTG(self.ser, self.address, 'N')[1:])
         while i < logs:
-            row      = telegram.createTG(self.ser, self.address, 'E')[1:]
+            row      = telegram.createTG(self.ser, self.address, arg)[1:]
             row      = row.decode('ascii')
             row      = row.split(';')
             date     = row[0][1:]
@@ -104,42 +91,74 @@ class combilog:
             for item in datalist:
                 element = telegram.hexIEE_dec(item)
                 tmpList.append(element)
-            
+
             tmpList.insert(0, str(date))
             tmpList.insert(0, int(name))
             dftmp = pd.DataFrame([tmpList], columns = self.cnames)
-            df    = df.append(dftmp)          
+            df    = df.append(dftmp)
             i     += 1
-            print('reading log: ', i, ' of ', logs)
+            print(f'reading log: {i} of {logs}')
         return df
 
-    def del_stor(self):
+    def repeat_reading_pt1(self) -> pd.DataFrame:
+        df = combilog.read_bookings_pt1(self, 'F')
+        return df
+
+    def pointer2_tostart(self) -> None:
+        '''sets pointer 2 to start'''
+        tg = telegram.createTG(self.ser, self.address, 'c')
+        telegram.checkRe(tg)
+
+    def pointer2_to_date(self, date: datetime) -> None:
+        '''sets pointer 2 to a specific date and time'''
+        date = datetime.strftime(date, '%y%m%d%H%M%S')
+        tg   = telegram.createTG(self.ser, self.address, 'c', date)
+        telegram.checkRe(tg)
+
+    def pointer2_to_pos(self, pos: str) -> None:
+        '''sets pointer 1 to a specific position'''
+        tg = telegram.createTG(self.ser, self.address, 'c', pos)
+        telegram.checkRe(tg)
+
+    def read_bookings_pt2(self) -> pd.DataFrame:
         '''
-        deletes the logger storage can't be undone!
+        reads all bookings for the currently set pointer 2
+        returns a pandas dataframe with all data
         '''
+        df = combilog.read_bookings_pt1(self, 'e')
+        return df
+
+    def repeat_reading_pt2(self) -> pd.DataFrame:
+        df = combilog.read_bookings_pt1(self, 'f')
+        return df
+
+    def nr_logs(self) -> int:
+        '''checks how many logs are available with the currently set pointer'''
+        logs = int(telegram.createTG(self.ser, self.address, 'N')[1:])
+        return logs
+
+    def del_stor(self) -> None:
+        '''deletes the logger storage cannot be undone!'''
         tg = telegram.createTG(self.ser, self.address, 'C.ALL')
         telegram.checkRe(tg)
-    
-    def read_channel(self, channelNr):
+
+    def read_channel(self, channelNr: str) -> float:
         '''
         reads a specific channel on the logger
         channelNr must be type str and must have the format '01'-'20' or '80'-'BB'
         '''
         tg = telegram.createTG(self.ser, self.address, 'R', channelNr)
-        tg = tg.decode('ascii')[1:]
+        tg = float(tg.decode('ascii')[1:])
         return tg
 
-    def read_datetime(self):
-        '''
-        reads the current logger time
-        returns the time as datetime object
-        '''
+    def read_datetime(self) -> datetime:
+        '''reads the current logger time'''
         tg = telegram.createTG(self.ser, self.address, 'H')
         tg = tg.decode('ascii')[1:-1]
         tg = datetime.strptime(tg, '%y%m%d%H%M%S')
         return tg
 
-    def set_datetime(self, newTime = datetime.now()):
+    def set_datetime(self, newTime: datetime = datetime.now()) -> None:
         '''
         newTime = time to set the logger clock to. Default is the system time
         else newTime must be a datetime object
@@ -147,8 +166,115 @@ class combilog:
         newTime       = datetime.strftime(newTime, '%y%m%d%H%M%S')
         tg            = telegram.createTG(self.ser, self.address, 'G', newTime)
         telegram.checkRe(tg)
-    
-    def other_call(self, *argv):
+
+    def dev_id(self) -> dict:
+        '''reads the device identification'''
+        tg = telegram.createTG(self.ser, self.address, 'V')
+        tg = tg.decode('ascii')[1:]
+        device_id = {
+            'vendor_name': tg[0:9],
+            'model_name':  tg[10:17],
+            'hw_revision': tg[18:23],
+            'sw_revision': tg[24:28]
+        }
+        return device_id
+
+    def dev_info(self) -> dict:
+        '''reads the device information'''
+        tg = telegram.createTG(self.ser, self.address, 'S')
+        tg = tg.decode('ascii')[1:]
+        # remove trailing whitespace in location
+        device_info = {
+            'location':      tg[0:19].strip(),
+            'serial_number': tg[20:26],
+            'nr_channels':   tg[26:28]
+        }
+        return device_info
+
+    def dev_status(self) -> dict:
+        '''returns the channel and module status'''
+        # TODO: parse the bits to define the error
+        tg = telegram.createTG(self.ser, self.address, 'Z')
+        tg = tg.decode('ascii')[1:]
+        device_status = {
+            'channel_status': tg[0:8],
+            'module_status': tg[8:12]
+        }
+        return device_status
+
+    def channel_info(self, channelNr: str):
+        '''returns all channel information'''
+        # FIXME: ° = 0xb0 raises: UnicodeDecodeError: 'ascii' codec can't decode byte 0xb0 in position 25: ordinal not in range(128)
+        # TODO: Parse e.g. dataformat what means '3'
+        tg = telegram.createTG(self.ser, self.address, 'B', channelNr)
+        tg = tg.decode('ascii')[1:]
+
+        # channel type
+        channel_type = tg[0]
+        if channel_type == '0':
+            channel_type = 'empty channel (EM)'
+        elif channel_type == '1':
+            channel_type = 'analogue input channel (AR)'
+        elif channel_type == '2':
+            channel_type = 'arithmeic channel (AR)'
+        elif channel_type == '3':
+            channel_type = 'digital output channel (DO)'
+        elif channel_type == '4':
+            channel_type = 'digital input channel (DI)'
+        elif channel_type == '5':
+            channel_type = 'setpoint channel (VO)',
+        elif channel_type == '6':
+            channel_type = 'alarm channel (AL)'
+        else:
+            channel_type = 'unknown channel type'
+
+        # type of calcualtion
+        type_of_calculation = tg[31]
+        if type_of_calculation == '0':
+            type_of_calculation = 'normal calculation of average value'
+        elif type_of_calculation == '1':
+            type_of_calculation = 'calculation of average value with wind direction'
+        elif type_of_calculation == '2':
+            type_of_calculation = 'calculation of the sum over the averaging interval'
+        elif type_of_calculation == '3':
+            type_of_calculation = 'continuous sum'
+        elif type_of_calculation == '4':
+            type_of_calculation = 'vectorial average for wind velocity'
+        elif type_of_calculation == '5':
+            type_of_calculation = 'vectorial average for wind direction',
+        else:
+            type_of_calculation = 'unknown type of calculation'
+
+        channel_information = {
+            'channel_type':     channel_type,
+            'channel_notation': tg[1:20].strip(),
+            'data_format': tg[21],
+            'field_length': tg[22],
+            'decimals': tg[23],
+            'unit': tg[24:29].strip(),
+            'channel_configuration': tg[30],
+            'type_of_calculation': type_of_calculation
+        }
+        return channel_information
+
+    def get_rate(self) -> str:
+        tg = telegram.createTG(self.ser, self.address, 'X')
+        tg = tg.decode('ascii')[1:]
+        rates = {
+            'measuring_rate':     tg[0:2],
+            'averaging_interval': tg[2:7]
+        }
+        return rates
+
+    def set_rate(self, rate: str) -> None:
+        '''
+        rate must be a str with len = 7
+        first two chars are measuring rate in s last 5 are averaging rate in s
+        '''
+        tg = telegram.createTG(self.ser, self.address, 'Y', rate)
+        telegram.checkRe(tg)
+
+    def other_call(self, *argv: str) -> str:
         '''
         method to use for calls that are not defined in this class
         the data is returned as string and needs further processing
